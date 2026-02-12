116c1866064d16683dd07d8cc0e9cc994900d7a1
Andres Hidalgo <andresfelipehb2003@gmail.com>
2026-02-11 23:56:38 -0500

    Módulo read_img.py se agregan las funciones relacionadas

---
+++ src/processing/read_img.py
@@ -0,0 +1,30 @@
+import os
+from typing import Tuple
+
+import cv2
+import numpy as np
+import pydicom
+from PIL import Image
+
+
+def read_dicom_file(path):
+    
+    img = pydicom.dcmread(path) # Ref: REFACTOR_SUMMARY.md - Punto 2
+    img_array = img.pixel_array
+    img2show = Image.fromarray(img_array)
+    img2 = img_array.astype(float)
+    img2 = (np.maximum(img2, 0) / img2.max()) * 255.0
+    img2 = np.uint8(img2)
+    img_RGB = cv2.cvtColor(img2, cv2.COLOR_GRAY2RGB)
+    return img_RGB, img2show
+
+
+def read_jpg_file(path):
+    img = cv2.imread(path)
+    img_array = np.asarray(img)
+    img2show = Image.fromarray(img_array)
+    img2 = img_array.astype(float)
+    img2 = (np.maximum(img2, 0) / img2.max()) * 255.0
+    img2 = np.uint8(img2)
+    return img2, img2show
+    
